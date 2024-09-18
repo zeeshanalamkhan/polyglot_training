03fc3c3537e5522893d6a19a1935fa91c6e72b6c
shail <sushma.shail@gmail.com>
2024-09-18 16:22:01 +0100

    adding quiz

--- golang/Basics/Day2/hello.go
+++ golang/Basics/Day2/hello.go
@@ -2,6 +2,14 @@ package main
 
 import "fmt"
 
+// variable constant
+var greet string
+var name = "learning@lbg"
+
 func main() {
-	fmt.Println("Hello learning@lbg day2")
+	fmt.Println("greet is " + greet)
+	greet = "hello "
+	// declare local variable
+	msg := greet + name
+	fmt.Println(msg)
 }
